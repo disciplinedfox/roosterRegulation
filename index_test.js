8b5eb6746d65d21aae617e4cb2dfe80a39b79f81
Andrew Bloom <105402149+disciplinedfox@users.noreply.github.com>
2023-09-28 18:33:39 -0700

    Add index.js contents and beginning of test code

---
+++ index_test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const Rooster = require('../index');
+
+describe('Rooster', () => {
+  describe('.announceDawn', () => {
+    it('returns a rooster call', () => {
+      //setup
+      const expected = 'cock-a-doodle-doo!';
+      //exercise
+      //verify
+    })
+  })
+})
