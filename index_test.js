5aec48114b30bda1b02f51e866351fa08b7d5840
Andrew Bloom <105402149+disciplinedfox@users.noreply.github.com>
2023-09-28 19:01:41 -0700

    uploaded finalized test

--- index_test.js
+++ index_test.js
@@ -7,7 +7,40 @@ describe('Rooster', () => {
       //setup
       const expected = 'cock-a-doodle-doo!';
       //exercise
+      const actual = Rooster.announceDawn();
       //verify
+      assert.equal(actual, expected);
+    });
+  });
+  describe('.timeAtDawn', () => {
+    it('returns its argument as a string', () => {
+      //setup
+      const input = 12;
+      const expected = '12';
+      //exercise
+      const actual = Rooster.timeAtDawn(input);
+      //verify
+      assert.strictEqual(actual, expected);
+    }),
+    it ('throws a rangeError if passed a number less than zero', () => {
+      //setup
+      const test = -1;
+      const expected = RangeError;
+      //exercise
+      //verify
+      assert.throws( () => {
+        Rooster.timeAtDawn(test)
+      }, expected)
+    }),
+    it ('throws a rangeError if passed a number greater than 24', () => {
+      //setup
+      const test = 25;
+      const expected = RangeError;
+      //exercise
+      //verify
+      assert.throws( () => {
+        Rooster.timeAtDawn(test)
+      }, expected)
     })
   })
-})
+});
